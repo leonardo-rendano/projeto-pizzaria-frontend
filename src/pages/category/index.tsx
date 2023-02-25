9f6a9373da0e2e23de14bb788ccfa8edb05119b5
Leonardo Campanhão Rendano <leonardo.rendano@gmail.com>
2023-02-25 15:13:41 -0300

    category service integration

--- src/pages/category/index.tsx
+++ src/pages/category/index.tsx
@@ -2,6 +2,8 @@ import { FormEvent, useState } from 'react'
 import styles from './style.module.scss'
 import Head from 'next/head'
 import { Header } from '@/components/Header'
+import { setupAPIClient } from '../../services/api'
+import { toast } from 'react-toastify'
 
 export default function Category() {
   const [name, setName] = useState('')
@@ -9,7 +11,17 @@ export default function Category() {
   async function handleRegister(event: FormEvent) {
     event.preventDefault()
 
-    alert('Categoria ' + name)
+    if (name === '') {
+      toast.error('Insira o nome de uma categoria!')
+    }
+
+    const apiClient = setupAPIClient()
+    await apiClient.post('category', {
+      name: name
+    })
+
+    toast.success('Categoria cadastrada com sucesso!')
+    setName('')
   }
 
   return (
