d1f643045b11a696278b5358bb63f4269b7cf8df
Leonardo Campanhão Rendano <leonardo.rendano@gmail.com>
2023-02-28 00:34:59 -0300

    Finish project

--- src/components/ModalOrder/index.tsx
+++ src/components/ModalOrder/index.tsx
@@ -6,10 +6,11 @@ import { OrderItemProps } from '../../pages/dashboard'
 interface ModalOrderProps {
   isOpen: boolean,
   onRequestClose: () => void,
-  order: OrderItemProps[]
+  order: OrderItemProps[],
+  handleFinishOrder: (id: string) => void 
 }
 
-export function ModalOrder({ isOpen, onRequestClose, order }: ModalOrderProps) {
+export function ModalOrder({ isOpen, onRequestClose, order, handleFinishOrder }: ModalOrderProps) {
 
   const customStyles = {
     content: {
@@ -51,7 +52,10 @@ export function ModalOrder({ isOpen, onRequestClose, order }: ModalOrderProps) {
           </section>
         ))}
 
-        <button className={styles.buttonOrder} onClick={() => {}}>
+        <button
+          className={styles.buttonOrder}
+          onClick={() => handleFinishOrder(order[0].order_id)}
+        >
           Concluir pedido
         </button>
       </div>
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -7,6 +7,7 @@ import { setupAPIClient } from '../../services/api'
 import Modal from 'react-modal'
 import { ModalOrder } from "@/components/ModalOrder" 
 import styles from './styles.module.scss'
+import { toast } from "react-toastify"
 
 type OrderProps = {
   id: string,
@@ -64,6 +65,28 @@ export default function Dashboard({ orders }: HomeProps) {
     setModalVisible(true)
   }
 
+  async function handleFinishItem(id: string) {
+    const apiClient = setupAPIClient()
+
+    await apiClient.put('/order/finish', {
+      order_id: id
+    })
+
+    const response = await apiClient.get('/orders')
+
+    toast.success('Pedido concluído!')
+
+    setOrderList(response.data)
+    setModalVisible(false)
+  }
+
+  async function handleRefreshOrders() {
+    const apiClient = setupAPIClient()
+
+    const response = await apiClient.get('/orders')
+    setOrderList(response.data)
+  }
+
 
   return (
     <>
@@ -76,12 +99,15 @@ export default function Dashboard({ orders }: HomeProps) {
         <main className={styles.container}>
           <div className={styles.containerHeader}>
             <h1>Últimos pedidos</h1>
-            <button>
+            <button onClick={handleRefreshOrders}>
               <FiRefreshCcw size={25} color="#3fffa3" />
             </button>
           </div>
 
           <article className={styles.listOrders}>
+            
+            {orderList.length === 0 && <span className={styles.emptyList}>Nenhum pedido no momento...</span>}
+
             {orderList.map(item => {
               return (
                 <section key={item.id} className={styles.orderItem}>
@@ -100,6 +126,7 @@ export default function Dashboard({ orders }: HomeProps) {
             isOpen={modalVisible}
             onRequestClose={handleCloseModal}
             order={modalItem}
+            handleFinishOrder={handleFinishItem}
           />)
         }
 
@@ -112,8 +139,6 @@ export const getServerSideProps = canSSRAuth(async (context) => {
   const apiClient = setupAPIClient(context)
 
   const response = await apiClient.get('/orders')
-
-  console.log(response.data)
 
   return {
     props: {
